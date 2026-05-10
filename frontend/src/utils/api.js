184d9b46b67b1104670f859a7d0f3b46b8fd139b
baidooanthony562 <baidooanthony562@gmail.com>
2026-05-10 12:10:33 +0000

    Update API base URL to Render backend

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://backend-9m2y.onrender.com/api',
   headers: { 'Content-Type': 'application/json' },
 });
 
